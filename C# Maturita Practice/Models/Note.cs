3584468a3c775dc368db9ea50223fb13788d789e
Tadeáš Udatný <udatnyt21@spstrutnov.cz>
2025-03-13 08:33:10 +0100

    User and Note models

---
+++ C# Maturita Practice/Models/Note.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C__Maturita_Practice.Models
+{
+    public class Note
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public DateTime Time { get; set; }
+        [Required]
+        public bool Important { get; set; }
+
+
+    }
+}
---
+++ C# Maturita Practice/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C__Maturita_Practice.Models
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required] 
+        public string Password { get; set;}
+    }
+}
