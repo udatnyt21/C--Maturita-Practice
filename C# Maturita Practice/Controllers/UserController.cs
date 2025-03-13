ff83e3f29061eca4901646ee2addb3d5b5eed29e
Tadeáš Udatný <udatnyt21@spstrutnov.cz>
2025-03-13 08:56:37 +0100

    User controller struktura

---
+++ C# Maturita Practice/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using C__Maturita_Practice.Models;
+using C__Maturita_Practice.Data;
+
+namespace C__Maturita_Practice.Controllers
+{
+    public class UserController : Controller
+    {
+        MyDBContext database;
+
+        public UserController(MyDBContext context)
+        {
+            database = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            
+        }
+        [HttpPost]
+        public IActionResult Register(string Name, string Password, string PasswordCheck , bool AIConsent)
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+
+        }
+        [HttpPost]
+        public IActionResult Login(string Name, string Password)
+        {
+
+        }
+
+
+    }
+}
