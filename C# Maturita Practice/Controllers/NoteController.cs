fa288cbc7cbd2b72e7f1c33949a9506e329022f9
Tadeáš Udatný <udatnyt21@spstrutnov.cz>
2025-03-20 13:56:51 +0100

    Notes Controller Structure

---
+++ C# Maturita Practice/Controllers/NoteController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace C__Maturita_Practice.Controllers
+{
+    public class NoteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(String Title, String Text, String isImportant)
+        {
+            return RedirectToAction("Profile", "User");
+        }
+        [HttpPost]
+        public IActionResult Delete()
+        {
+            return RedirectToAction("Profile", "User");
+        }
+        [HttpPost]
+        public IActionResult SetImportant()
+        {
+            return RedirectToAction("Profile", "User");
+        }
+    }
+}
